56b669e6c0aac78b47aa95a5299ce4fc5df13e04
tkachenkosi <tkachenkosi@yandex.ru>
2025-01-09 06:54:38 +0300

    ДЗ Управление потоком

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,31 +4,115 @@ import (
 	"fmt"
 )
 
+const (
+	ERR_MSG = "Не верный код валюты. Повторите ввод."
+)
+
 func main() {
-	// const (
-	// 	USD_EUR float64 = 0.9667
-	// 	USD_RUB float64 = 101.68
-	// )
+	fmt.Println("КОНВЕРТОР ВАЛЮТ")
+	valIn := getValIn()
+	summa := getSum()
+	valOut := getValOut(valIn)
 
-	USD_EUR, USD_RUB := getUserInput()
-
-	EUR_RUB := USD_RUB / USD_EUR
-	fmt.Println("EUR в RUB =", EUR_RUB)
+	calculateCurs(summa, valIn, valOut)
 }
 
-func getUserInput() (float64, float64) {
-	var usd_eur float64
-	var usd_rub float64
-
-	fmt.Print("Введите курс для USD EUR: ")
-	fmt.Scan(&usd_eur)
-	fmt.Print("Введите курс для USD RUB: ")
-	fmt.Scan(&usd_rub)
-
-	return usd_eur, usd_rub
+func codeVal(codeNum int) string {
+	switch codeNum {
+	case 1:
+		return "USD"
+	case 2:
+		return "EUR"
+	case 3:
+		return "RUS"
+	default:
+		return "ERR"
+	}
 }
 
-func calculateCurs(sum float64, val1, val2 int) float64 {
-	fmt.Println(val1, val2)
-	return sum
+func getValIn() int {
+	var val int
+
+	for {
+		fmt.Print("Введите исходную валюту 1-USD 2-EUR 3-RUB: ")
+		fmt.Scan(&val)
+
+		if val >= 1 && val <= 3 {
+			return val
+		} else {
+			fmt.Println(ERR_MSG)
+		}
+	}
+}
+
+func getValOut(valIn int) int {
+	var val int
+	var listCodeVal string
+
+	// убираем из списка код исходной валюты
+	for i := 1; i < 4; i++ {
+		if i != valIn {
+			listCodeVal = fmt.Sprintf("%s %d-%s", listCodeVal, i, codeVal(i))
+		}
+	}
+
+	for {
+		fmt.Print("Введите целевую валюту", listCodeVal, ": ")
+		fmt.Scan(&val)
+
+		if val < 1 && val > 3 {
+			fmt.Println(ERR_MSG)
+			continue
+		}
+
+		if val == valIn {
+			fmt.Println("Исходная и целевая валюта должны быть разными. Повторите ввод.")
+		} else {
+			return val
+		}
+
+	}
+}
+
+func getSum() float64 {
+	var sum float64
+
+	for {
+		fmt.Print("Введите сумму для конвертации: ")
+		fmt.Scan(&sum)
+
+		if sum <= 0 {
+			fmt.Println("Не верное значение суммы. Повторите ввод.")
+		} else {
+			return sum
+		}
+	}
+}
+
+func calculateCurs(sumIn float64, valIn, valOut int) {
+	// заданы курсы двух базовых пар валют
+	const (
+		USD_EUR float64 = 0.9667
+		USD_RUB float64 = 101.68
+	)
+
+	var valCurs float64
+
+	switch {
+	case valIn == 1 && valOut == 2:
+		valCurs = USD_EUR
+	case valIn == 1 && valOut == 3:
+		valCurs = USD_RUB
+	case valIn == 2 && valOut == 1:
+		valCurs = 1.0 / USD_EUR
+	case valIn == 2 && valOut == 3:
+		valCurs = USD_RUB / USD_EUR
+	case valIn == 3 && valOut == 1:
+		valCurs = 1.0 / USD_RUB
+	case valIn == 3 && valOut == 2:
+		valCurs = USD_EUR / USD_RUB
+	}
+
+	fmt.Printf("Результат конвертации %.2f %s в %s = %.2f по кросс-курсу %.5f\n",
+		sumIn, codeVal(valIn), codeVal(valOut), sumIn*valCurs, valCurs)
 }
